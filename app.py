5279d09303d892c145cbf3dd61ab815bace2249b
monacoding <symphony5809@gmail.com>
2025-04-19 05:50:57 +0900

    초기 화면 구성

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
 
+# .env 파일 로드
+load_dotenv()
 
-PORT=5000
+# Flask 앱 생성
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    HOST = os.getenv("FLASK_HOST")
+    PORT = int(os.getenv("FLASK_PORT"))
+    DEBUG_MODE = os.getenv("FLASK_DEBUG", "True").lower() == "true"
+
+    app.run(host=HOST, port=PORT, debug=DEBUG_MODE) 
